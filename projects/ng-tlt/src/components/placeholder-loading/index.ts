acb5252f8d80568aa35facdeccaf5c230582fac8
I'm Tung <toilatung@icloud.com>
2019-09-17 22:09:32 +0700

    Revert "Fix"
    This reverts commit f9aad8c63168b171f4ab3ca36014139b33590832.

--- projects/ng-tlt/src/components/placeholder-loading/index.ts
+++ projects/ng-tlt/src/components/placeholder-loading/index.ts
@@ -1,1 +1,4 @@
-export * from "./public-api";
+export * from "./pl-main.module";
+export * from "./pl-main.component";
+export * from "./shape/shape-circle.component";
+export * from "./shape/shape-rectangle.component";
